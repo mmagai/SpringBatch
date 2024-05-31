40b0938e31b3c4dc3ca59b6babb486aca26f1f74
magaimahesh <magaimahesh@gmail.com>
2024-05-31 12:56:27 -0700

    Add Job Parameter Increment code

--- src/main/java/com/example/config/SampleJob.java
+++ src/main/java/com/example/config/SampleJob.java
@@ -5,6 +5,7 @@ import org.springframework.batch.core.Step;
 import org.springframework.batch.core.StepContribution;
 import org.springframework.batch.core.configuration.annotation.JobBuilderFactory;
 import org.springframework.batch.core.configuration.annotation.StepBuilderFactory;
+import org.springframework.batch.core.launch.support.RunIdIncrementer;
 import org.springframework.batch.core.scope.context.ChunkContext;
 import org.springframework.batch.core.step.tasklet.Tasklet;
 import org.springframework.batch.repeat.RepeatStatus;
@@ -32,6 +33,7 @@ public class SampleJob {
 		 return JobBuilderFactory.get("First Job")
 		.start(firstStep())
 		.next(secondStep())
+		.incrementer(new RunIdIncrementer())
 		.build();
 		
 		
